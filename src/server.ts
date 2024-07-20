8280c3110d972635d315cc5f3af39ccf58f4f839
Micael Miranda <66328408+micaelmi@users.noreply.github.com>
2024-07-20 16:34:02 -0300

    api full setup

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import cors from "@fastify/cors";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env";
+import { errorHandler } from "./error-handler";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "Fly TM API",
+      description: "API para o app de tênis de mesa Fly TM",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler(errorHandler);
+
+// app.register();
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server flying! 🚀");
+});
