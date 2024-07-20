d625a6193fc04242c5da1c5643c8fb8813a79de3
Micael Miranda <66328408+micaelmi@users.noreply.github.com>
2024-07-20 18:10:48 -0300

    create database schema

---
+++ prisma/migrations/20240720210607_create_tables_v1/migration.sql
@@ -0,0 +1,322 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('active', 'inactive');
+
+-- CreateEnum
+CREATE TYPE "CreditActions" AS ENUM ('spend', 'buy');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "bio" TEXT,
+    "training_days" INTEGER NOT NULL DEFAULT 0,
+    "credits" INTEGER NOT NULL DEFAULT 0,
+    "state" TEXT,
+    "city" TEXT,
+    "instagram" TEXT,
+    "image_url" TEXT,
+    "level_id" INTEGER NOT NULL,
+    "game_style_id" INTEGER NOT NULL,
+    "club_id" INTEGER NOT NULL,
+    "user_type_id" INTEGER NOT NULL,
+    "hand_grip_id" INTEGER NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tokens" (
+    "id" SERIAL NOT NULL,
+    "number" INTEGER NOT NULL,
+    "user_email" TEXT NOT NULL,
+    "expiration" TIMESTAMP(3) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'active',
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "trainings" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "time" INTEGER NOT NULL,
+    "icon_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "level_id" INTEGER NOT NULL,
+
+    CONSTRAINT "trainings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "strategies" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "how_it_works" TEXT NOT NULL,
+    "icon_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "level_id" INTEGER NOT NULL,
+
+    CONSTRAINT "strategies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "strategy_items" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "strategy_id" TEXT NOT NULL,
+    "movement_id" INTEGER NOT NULL,
+
+    CONSTRAINT "strategy_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "training_items" (
+    "id" SERIAL NOT NULL,
+    "counting_mode" TEXT NOT NULL,
+    "reps" INTEGER NOT NULL,
+    "time" INTEGER NOT NULL,
+    "queue" INTEGER NOT NULL,
+    "comments" TEXT NOT NULL,
+    "training_id" TEXT NOT NULL,
+    "movement_id" INTEGER NOT NULL,
+
+    CONSTRAINT "training_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "movements" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "average_time" INTEGER NOT NULL,
+    "video_url" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+
+    CONSTRAINT "movements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "clubs" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "logo_url" TEXT NOT NULL,
+    "background" TEXT NOT NULL,
+    "owner_username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "instagram" TEXT,
+    "other_contacts" TEXT,
+    "schedule" TEXT NOT NULL,
+    "prices" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "address_number" INTEGER,
+    "complement" TEXT,
+    "maps_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "clubs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "events" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "show_date" TIMESTAMP(3) NOT NULL,
+    "hide_date" TIMESTAMP(3) NOT NULL,
+    "cep" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "address_number" INTEGER,
+    "complement" TEXT,
+    "maps_url" TEXT NOT NULL,
+    "level" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "match_history" (
+    "id" SERIAL NOT NULL,
+    "player1" TEXT NOT NULL,
+    "player2" TEXT NOT NULL,
+    "better_of" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "sets_player1" INTEGER NOT NULL,
+    "sets_player2" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "match_history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "game_history" (
+    "id" SERIAL NOT NULL,
+    "points_player1" INTEGER NOT NULL,
+    "points_player2" INTEGER NOT NULL,
+    "game_number" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "match_history_id" INTEGER NOT NULL,
+
+    CONSTRAINT "game_history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "credit_transactions" (
+    "id" SERIAL NOT NULL,
+    "action" "CreditActions" NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "credit_transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contacts" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "answer" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "contact_type_id" INTEGER NOT NULL,
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_types" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "user_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "levels" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "levels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "game_styles" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "game_styles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "hand_grips" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "hand_grips_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contact_types" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "contact_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_level_id_fkey" FOREIGN KEY ("level_id") REFERENCES "levels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_game_style_id_fkey" FOREIGN KEY ("game_style_id") REFERENCES "game_styles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_club_id_fkey" FOREIGN KEY ("club_id") REFERENCES "clubs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_user_type_id_fkey" FOREIGN KEY ("user_type_id") REFERENCES "user_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_hand_grip_id_fkey" FOREIGN KEY ("hand_grip_id") REFERENCES "hand_grips"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trainings" ADD CONSTRAINT "trainings_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "trainings" ADD CONSTRAINT "trainings_level_id_fkey" FOREIGN KEY ("level_id") REFERENCES "levels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strategies" ADD CONSTRAINT "strategies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strategies" ADD CONSTRAINT "strategies_level_id_fkey" FOREIGN KEY ("level_id") REFERENCES "levels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strategy_items" ADD CONSTRAINT "strategy_items_strategy_id_fkey" FOREIGN KEY ("strategy_id") REFERENCES "strategies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "strategy_items" ADD CONSTRAINT "strategy_items_movement_id_fkey" FOREIGN KEY ("movement_id") REFERENCES "movements"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "training_items" ADD CONSTRAINT "training_items_training_id_fkey" FOREIGN KEY ("training_id") REFERENCES "trainings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "training_items" ADD CONSTRAINT "training_items_movement_id_fkey" FOREIGN KEY ("movement_id") REFERENCES "movements"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "match_history" ADD CONSTRAINT "match_history_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "game_history" ADD CONSTRAINT "game_history_match_history_id_fkey" FOREIGN KEY ("match_history_id") REFERENCES "match_history"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "credit_transactions" ADD CONSTRAINT "credit_transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contacts" ADD CONSTRAINT "contacts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contacts" ADD CONSTRAINT "contacts_contact_type_id_fkey" FOREIGN KEY ("contact_type_id") REFERENCES "contact_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
